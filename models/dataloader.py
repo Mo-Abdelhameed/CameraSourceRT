13ceb27de26175e165df99b1ade132d02cea207e
Nakul Ravindra Padalkar <2807210+nakulpadalkar@users.noreply.github.com>
2024-01-18 19:01:54 -0500

    complete resnet 18 run.

--- models/dataloader.py
+++ models/dataloader.py
@@ -10,7 +10,7 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm  # Import tqdm for progress bars
 
 # Configure logging
-logging.basicConfig(filename='training.log', level=logging.INFO,
+logging.basicConfig(filename='../logs/training.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s: %(message)s')
 
 
